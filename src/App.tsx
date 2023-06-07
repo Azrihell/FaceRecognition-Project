bab241c598b0e60693f158ab453ebd46a8bb9afb
Azriel <azrihell.l@gmail.com>
2023-06-07 14:55:13 -0500

    added rank, fun background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './App.css'
+import ParticlesBg from 'particles-bg'
 import { Navigation } from './components/Nav/Navigation'
 import { Logo } from './components/Logo/Logo'
 import { ImageLinkForm } from './components/ImageLinkForm/ImageLinkForm'
+import { Rank } from './components/Rank/Rank'
 
 export const App = () => {
   return (
+   
     <div className='App'>
+      <ParticlesBg type="square" num ={15} bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     {/* <FaceRecognition /> */}
 
