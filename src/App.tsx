9f21e2e972617534231ccf26a4e912f09a45f813
Azriel <azrihell.l@gmail.com>
2023-06-12 20:53:27 -0500

    added API

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,49 @@ import { Rank } from './components/Rank/Rank'
 
 export const App = () => {
   const [input, setInput] = React.useState<string>('')
-  const [toggle, setToggle] = React.useState<boolean>(true)
 
   React.useEffect(() => {
     console.log(input)
-  }, [toggle,input]);
+  }, [input]);
 
   const onButtonSubmit = () => {
     console.log('clicked hehe')
-    setToggle(!toggle)
+
+    // URL of image to use. Change this to your image.
+    const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": "clarifai",
+        "app_id": "main"
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": IMAGE_URL
+            }
+          }
+        }
+      ]
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + '64e7800599d34839bae1043eed95cde5',
+        'Content-Type': 'application/json'
+      },
+      body: raw
+    };
+
+    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/b8fa05a04e0649a1a9d3186b246a59b3/outputs`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error))
   }
+
 
   return (
 
