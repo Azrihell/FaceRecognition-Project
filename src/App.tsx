308487045a13a2f261fd1fd8d073a018f88a1500
Azriel <azrihell.l@gmail.com>
2023-06-15 22:00:54 -0500

    setup for bounding box

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,28 @@ import { FaceRecognition } from './components/FaceRecognition/FaceRecognition'
 export const App = () => {
   const [input, setInput] = React.useState<string>('')
   const [imageUrl, setImageUrl] = React.useState<string>('')
+  const [box, setBox] = React.useState('')
+
+  const calcFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.rightCol * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  const displayFaceBox = (box) => {
+    ({box: setBox})
+  }
 
   React.useEffect(() => {
-    console.log("NEW IMAGE ADDED")
+    console.log()
   }, [input]);
 
   const onButtonSubmit = () => {
@@ -46,7 +65,8 @@ export const App = () => {
     };
 
     fetch(`https://api.clarifai.com/v2/models/face-detection/versions/b8fa05a04e0649a1a9d3186b246a59b3/outputs`, requestOptions)
-      .then(response => response.text())
+      .then(response => displayFaceBox(calcFaceLocation(response.text)))
+      // .then(response => response.text())
       .then(result => console.log(result))
       .catch(error => console.log('error', error))
   }
