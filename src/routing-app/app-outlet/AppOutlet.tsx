0ebc37aa52ff7c75cda7cf00974be03c5c736c11
Azriel <azrihell.l@gmail.com>
2023-08-30 13:11:55 -0500

    changed build command for deployment

--- src/routing-app/app-outlet/AppOutlet.tsx
+++ src/routing-app/app-outlet/AppOutlet.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Outlet } from "react-router"
-import { Navigation } from "../../components/Nav/Navigation"
+// import { Navigation } from "../../components/Nav/Navigation"
 
 // Application Screen || Define Exports
 // =================================================================================================
